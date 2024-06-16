3172d9dc7e392081f0fed8c28b61476a2dad7c6b
tonniegeetz <tonniegeetz@gmail.com>
2024-06-16 23:19:42 +0300

    to_s

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0].to_s
+  elsif array.length == 2
+    array.join(' and ').to_s
+  else
+    last = array[0..-2].join(', ') + ', and ' + array[-1]
+    last.to_s
+  end
 end
